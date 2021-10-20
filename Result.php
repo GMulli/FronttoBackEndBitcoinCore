cf7144eec94417311140a1d5be17ce3fef4db231
Gmulli <31466479+GMulli@users.noreply.github.com>
2021-10-20 10:48:23 -0400

    Update Result.php

--- Result.php
+++ Result.php
@@ -1,13 +1,12 @@
 <?php
-
 #THIS IS A VERY VERY QUICK AND DIRTY METHOD OF DOING A CURL CALL TO LOCALHOST FOR GETBLOCKHASH
 
 $op = $_GET["operation"];
 $num = $_GET["num"];
 
-    if ($op == "getblockhash" && $num > 0 && isset($_GET["operation"]) && isset($_GET["num"])) {
+    if ($op == "getblockhash" && $num > 0 && is_numeric($num) && isset($_GET["operation"]) && isset($_GET["num"])) {
         $com = "./bitcoin-cli " . $op . " " . $num;
-        $result = "curl --data-binary '{\"jsonrpc\":\"1.0\",\"id\":\"curltext\",\"method\":\"getblockhash\",\"params\":[2]}' -H 'content-type:text/plain;' http://a:a@127.0.0.1:8332/";
+        $result = "curl --data-binary '{\"jsonrpc\":\"1.0\",\"id\":\"curltext\",\"method\":\"" . $op . "\",\"params\":[" . $num . "]}' -H 'content-type:text/plain;' http://a:a@127.0.0.1:8332/";
         $a = json_decode(shell_exec($result),true);
         echo "result: " . $a['result'];
     }
